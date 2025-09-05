d1b4e30475a985d053a1baa029c6bb9861d9b068
Shahin Ghanei <shahin.ghanei@postcode-lotterie.de>
2025-09-05 14:46:49 +0200

    set up shadcn ui

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import { Button } from './components/ui/button';
 
 function App() {
   const [message, setMessage] = useState('');
@@ -9,7 +10,14 @@ function App() {
       .then((data) => setMessage(data.message));
   }, []);
 
-  return <p className="text-red-500 p-4 text-3xl font-bold">{message}</p>;
+  return (
+    <div className="py-10 flex flex-col justify-center items-center">
+      <p className="text-red-500 p-4 text-3xl font-bold text-center">
+        {message}
+      </p>
+      <Button className="w-1/2">CLICK ME</Button>
+    </div>
+  );
 }
 
 export default App;
